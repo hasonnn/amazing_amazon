7d46ed3cf76d5530c1c1b2cad5542a83e7a6d239
Jason Chan <jchan321738@gmail.com>
2021-04-19 21:09:17 -0700

    create about page add navlinks

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get('/', {to: 'home#index', as: 'root'})
+  get('/about', {to: 'home#about', as: 'about'})
 
 end
