3e35e20667ceaeb4ab4fd44b6bde789d2bd111fe
Jason Chan <jchan321738@gmail.com>
2021-04-19 21:54:18 -0700

    create model productdb migrate faker seeds

---
+++ app/models/product.rb
@@ -0,0 +1,20 @@
+class Product < ApplicationRecord
+
+    DEFAULT_PRICE = 1
+
+    before_validation :set_default_price
+    before_save :capitalize_title
+
+    validates :title, presence: true, uniqueness: true
+    validates :price, numericality: { greater_than: 0 }
+    validates :description, presence: true, length: { minimum: 2 }
+
+    private
+    def set_default_price
+        self.price ||= DEFAULT_PRICE
+    end
+    
+    def capitalize_title
+        self.title.capitalize!
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+200.times do
+    created_at = Faker::Date.backward(days:365 * 5)
+
+    Product.create(
+        title: Faker::WorldCup.city,
+        description: Faker::ChuckNorris.fact,
+        price: rand(100_000)
+    )
+end
+
+items = Product.all
+
+puts "Generated #{items.count} items"
